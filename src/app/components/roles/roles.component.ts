07dfa98da084c7e472f685ae80cdf1439f7ed011
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-01-20 18:45:48 -0300

    33:25

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css',
+})
+export class RolesComponent {
+  firstname: string = 'Angular Tutorial';
+  angularVersion = 'Version 18';
+
+  version: number = 18;
+
+  isActive: boolean = false;
+
+  currentDate: Date = new Date();
+
+  inputType: string = 'button';
+}
