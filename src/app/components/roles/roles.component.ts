810c34dfac0832a5cf700883f5f708641b35a72c
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-01-20 20:43:11 -0300

    42:00

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-roles',
-  standalone: true,
-  imports: [],
-  templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css',
+    selector: 'app-roles',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './roles.component.html',
+    styleUrl: './roles.component.css',
 })
 export class RolesComponent {
-  firstname: string = 'Angular Tutorial';
-  angularVersion = 'Version 18';
 
-  version: number = 18;
+    firstname: string = 'Angular Tutorial';
+    angularVersion = 'Version 18';
+    version: number = 18;
+    isActive: boolean = false;
+    currentDate: Date = new Date();
+    inputType: string = 'button';
+    selectedState: string = "";
 
-  isActive: boolean = false;
+    showWelcomeAlert() {
+        alert("Welcome to Angular 18 Tutorial");
+    }
 
-  currentDate: Date = new Date();
-
-  inputType: string = 'button';
+    showMessage(message: string) {
+        alert(message);
+    }
 }
